11345a923a9b15d05ae00d1ac18a4ea5811f6abd
Fan666s42s <x666s42@gmail.com>
2017-01-15 14:02:26 +0800

    Add files via upload

--- RPS.rb
+++ RPS.rb
@@ -1,39 +1,41 @@
 class RPS
+  attr_accessor :com_choose, :player_choose
+  def initialize
+    @com_choose = ["R","P","S"].sample
+    puts "=============================================="
+    puts "=     welcome to play Rock Paper Sicssor     ="
+    puts "=============================================="
+    puts "=please choose Rock(R) Paper(P) or Sicssor(S)="
+    @player_choose =  gets.chomp.upcase
+  end
 
-    attr_accessor :comChoose, :playerChoose
-
-    def initialize
-        @comChoose = ["R","P","S"].sample
-        puts "=============================================="
-        puts "=     welcome to play Rock Paper Sicssor     ="
-        puts "=============================================="
-        puts "=please choose Rock(R) Paper(P) or Sicssor(S)="
-        @playerChoose =  gets.chomp.upcase
+  def decide(a, b)
+    player_choose = a
+    com_choose = b
+    if player_choose != com_choose
+      case player_choose
+      when "R"
+        puts com_choose == "P" ? "You choose Rock, Computer Choose Paper. You lose!" : "You choose Rock, Computer Choose Sicssor. You WIN!"
+      when "P"
+        puts com_choose == "S" ? "You choose Paper, Computer Choose Sicssor. You lose!" : "You choose Paper, Computer Choose Rock. You WIN!"
+      when "S"
+        puts com_choose == "R" ? "You choose Sicssor, Computer Choose Rock. You lose!" : "You choose Sicssor, Computer Choose Paper. You WIN!"
+      else
+        puts "invalid  input, you lose!"
+      end
+    else
+      case player_choose
+      when "R"
+        puts "You choose Rock, Computer choose Rock. It's draw!"
+      when "P"
+        puts "You choose Paper,  Computer choose Paper.It's draw!"
+      when "S"
+        puts "You choose Sicssor,  Computer choose Sicssor.It's draw!"
+      else
+        puts "invalid  input, you lose!"
+      end
     end
 
-    def decide(a, b)
-        playerChoose = a
-        comChoose = b
-        if playerChoose != comChoose
-            case playerChoose
-            when "R"
-                puts comChoose == "P" ? "You choose Rock, Computer Choose Paper. You lose!" : "You choose Rock, Computer Choose Sicssor. You WIN!"
-            when "P"
-                puts comChoose == "S" ? "You choose Paper, Computer Choose Sicssor. You lose!" : "You choose Paper, Computer Choose Rock. You WIN!"
-            when "S"
-                puts comChoose == "R" ? "You choose Sicssor, Computer Choose Rock. You lose!" : "You choose Sicssor, Computer Choose Paper. You WIN!"
-            end
-        else
-            case playerChoose
-            when "R"
-                puts "You choose Rock, Computer choose Rock. It's draw!"
-            when "P"
-                puts "You choose Paper,  Computer choose Paper.It's draw!"
-            when "S"
-                puts "You choose Sicssor,  Computer choose Sicssor.It's draw!"
-            end
-        end
-
-    end
+  end
 
 end
--- RPSMain.rb
+++ RPSMain.rb
@@ -2,28 +2,15 @@ require_relative "RPS"
 
 begin 
 
-    game = RPS.new
+  game = RPS.new
 
-    game.decide(game.playerChoose,game.comChoose)
+  game.decide(game.player_choose,game.com_choose)
 
-    puts "do you want to play again(Y/N)"
-    playAgain = gets.chomp.upcase
+  puts "do you want to play again(Y/N)"
+  playAgain = gets.chomp.upcase
 end while playAgain == "Y"
     
 puts "=============================================="
 puts "=            Thanks for playing              ="
 puts "=               BY Fan666s42s                ="
 puts "=============================================="
-
-
-
-
-
-
-
-
-
-
-
-
-
